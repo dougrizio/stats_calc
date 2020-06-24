afc1900a1617f5163d6952f999c66883b65e1af8
drizles <dougrizio@gmail.com>
2020-06-24 17:53:42 -0400

    Fix: reordered calculations

--- src/Calculator.py
+++ src/Calculator.py
@@ -7,47 +7,47 @@ class Calculator:
         pass
 
     @staticmethod
-    def subtraction(a, b):
-        return int(b) - int(a)
-
-    @staticmethod
     def addition(a, b):
         return int(a) + int(b)
+
+    @staticmethod
+    def subtraction(a, b):
+        return int(b) - int(a)
 
     @staticmethod
     def multiplication(a, b):
         return int(a) * int(b)
 
     @staticmethod
-    def squaring(a):
-        return int(a) * int(a)
-
-    @staticmethod
     def division(a, b):
         return float(b) / float(a)
+
+    @staticmethod
+    def squaring(a):
+        return int(a) * int(a)
 
     @staticmethod
     def squarerooting(a):
         return math.sqrt(float(a))
 
-    def subtract(self, a, b):
-        self.result = Calculator.subtraction(a, b)
-        return self.result
-
     def add(self, a, b):
         self.result = Calculator.addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = Calculator.subtraction(a, b)
         return self.result
 
     def multiply(self, a, b):
         self.result = Calculator.multiplication(a, b)
         return self.result
 
-    def square(self, a):
-        self.result = Calculator.squaring(a)
-        return self.result
-
     def divide(self, a, b):
         self.result = Calculator.division(a, b)
+        return self.result
+
+    def square(self, a):
+        self.result = Calculator.squaring(a)
         return self.result
 
     def squareroot(self, a):
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -13,18 +13,18 @@ class MyTestCase(unittest.TestCase):
     def test_results_property(self):
         self.assertEqual(self.calculator.result, 0)
 
-    def test_subtraction(self):
-        test_subtraction_data = MyTestCase.CsvReader('/src/ut_subtraction.csv')
-        pprint(test_subtraction_data)
-        for row in test_subtraction_data:
-            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
-            self.assertEqual(self.calculator.result, int(row['Result']))
-
     def test_addition(self):
         test_addition_data = MyTestCase.CsvReader('/src/ut_addition.csv')
         pprint(test_addition_data)
         for row in test_addition_data:
             self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
+    def test_subtraction(self):
+        test_subtraction_data = MyTestCase.CsvReader('/src/ut_subtraction.csv')
+        pprint(test_subtraction_data)
+        for row in test_subtraction_data:
+            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_multiplication(self):
@@ -34,19 +34,19 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
-    def test_squaring(self):
-        test_squaring_data = MyTestCase.CsvReader('/src/ut_squaring.csv')
-        pprint(test_squaring_data)
-        for row in test_squaring_data:
-            self.assertEqual(self.calculator.square(row['Value 1']), int(row['Result']))
-            self.assertEqual(self.calculator.result, int(row['Result']))
-
     def test_division(self):
         test_division_data = MyTestCase.CsvReader('/src/ut_division.csv')
         pprint(test_division_data)
         for row in test_division_data:
             self.assertAlmostEqual(self.calculator.divide(row['Value 1'], row['Value 2']), float(row['Result']))
             self.assertAlmostEqual(self.calculator.result, float(row['Result']))
+
+    def test_squaring(self):
+        test_squaring_data = MyTestCase.CsvReader('/src/ut_squaring.csv')
+        pprint(test_squaring_data)
+        for row in test_squaring_data:
+            self.assertEqual(self.calculator.square(row['Value 1']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_squarerooting(self):
         test_squarerooting_data = MyTestCase.CsvReader('/src/ut_squarerooting.csv')
