bd383d41d20701e00c7f5dfedd779eff3423a20a
drizles <dougrizio@gmail.com>
2020-06-24 02:51:43 -0400

    Task: added subtraction statements

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,7 +1,12 @@
-
+def subtraction(a, b):
+    return int(b) - int(a)
 
 class Calculator:
     result = 0
 
     def __init__(self):
         pass
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -13,6 +13,12 @@ class MyTestCase(unittest.TestCase):
     def test_results_property(self):
         self.assertEqual(self.calculator.result, 0)
 
+    def test_subtraction(self):
+        test_data = CsvReader('/src/ut_subtraction.csv').data
+        pprint(test_data)
+        for row in test_data:
+            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
 
 if __name__ == '__main__':
     unittest.main()
