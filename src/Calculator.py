69945f60d0852438b40672105e782e6b2169c3ae
drizles <dougrizio@gmail.com>
2020-06-24 04:54:08 -0400

    Fix: tweaked staticmethod placements

--- src/Calculator.py
+++ src/Calculator.py
@@ -4,18 +4,6 @@ class Calculator:
     def __init__(self):
         pass
 
-    def subtract(self, a, b):
-        self.result = Calculator.subtraction(a, b)
-        return self.result
-
-    def add(self, a, b):
-        self.result = Calculator.addition(a, b)
-        return self.result
-
-    def multiply(self, a, b):
-        self.result = Calculator.multiplication(a, b)
-        return self.result
-
     @staticmethod
     def subtraction(a, b):
         return int(b) - int(a)
@@ -27,3 +15,15 @@ class Calculator:
     @staticmethod
     def multiplication(a, b):
         return int(a) * int(b)
+
+    def subtract(self, a, b):
+        self.result = Calculator.subtraction(a, b)
+        return self.result
+
+    def add(self, a, b):
+        self.result = Calculator.addition(a, b)
+        return self.result
+
+    def multiply(self, a, b):
+        self.result = Calculator.multiplication(a, b)
+        return self.result
