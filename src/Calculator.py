55b8b906a54f29666f341ad31438f482bd047eaa
drizles <dougrizio@gmail.com>
2020-06-24 03:14:07 -0400

    Fix: changed back into subtraction statements

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,5 +1,5 @@
-def addition(a, b):
-    return int(a) + int(b)
+def subtraction(a, b):
+    return int(b) - int(a)
 
 class Calculator:
     result = 0
@@ -7,6 +7,6 @@ class Calculator:
     def __init__(self):
         pass
 
-    def add(self, a, b):
-        self.result = addition(a, b)
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
         return self.result
